d771fbb54a0f27627ad2b2ed2f217e25d66698fd
lindawilliams <williams.linda1023@gmail.com>
2024-08-12 17:51:53 -0400

    updates

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = SareetaApplication.class)
 @SpringBootTest
 public class SareetaApplicationTests {
 
